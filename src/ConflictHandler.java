c8351b979ab91d421fd49f5dce68b891b76861ad
Matheus Carneiro <carneiromatheu@gmail.com>
2025-07-25 17:08:55 -0300

    refactor: remove unused getRegisters method and clean up code structure

--- src/ConflictHandler.java
+++ src/ConflictHandler.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
 
 public class ConflictHandler {
-    public static ArrayList<String> getRegisters(String instruction) throws NullPointerException{
-        final String regex =  """
-            \\$(?<register>
-              zero|at|v[01]|a[0-3]|t[0-9]|s[0-7]|
-              k[01]|gp|sp|fp|ra|pc|hi|lo
-            )\\b
-            """;
-        if(instruction == null  || instruction.isEmpty()) throw new NullPointerException("The instruction cannot be null!");
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(instruction);
-        ArrayList<String> registers = new ArrayList<>();
-        while (matcher.find()) {
-            registers.add(matcher.group());
+
+    private ConflictHandler() {
+    }
+
+    public static int check(List<ArrayList<String>> instructions) {
+        int bubbles = 0;
+
+        for (ArrayList<String> instruction : instructions) {
+            String opcode = getOpcode(instruction);
+            // Instruction inst = new Instruction(opcode, instruction);
+            // Ou utilizar de outra forma
         }
-        if(registers.isEmpty()) {
-            throw new NullPointerException("This instruction does not have any register!");
-        }
-        return registers;
+
+        return bubbles;
+    }
+
+    private static String getOpcode(ArrayList<String> instruction) {
+        return instruction.get(0);
     }
 }
